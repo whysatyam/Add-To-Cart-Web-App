f85ee976aff2ae47f0fbbff7b236431e0152d391
satyam <satyamremote@gmail.com>
2024-08-26 00:27:21 +0530

    commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebas
 import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
-    databaseURL:"https://realtime-database-43771-default-rtdb.asia-southeast1.firebasedatabase.app/"
+    databaseURL:"https://addtocart-87b36-default-rtdb.asia-southeast1.firebasedatabase.app/"
 }
 
 const app = initializeApp(appSettings)
